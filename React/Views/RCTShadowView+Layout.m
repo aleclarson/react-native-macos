facebook/react-native

Introduced -[RCTShadowView contentFrame]
Summary:
Another trivial sugar/helper to access computed by Yoga layout metrics.
We will use it soon in the new Text render/layout engine.

Reviewed By: mmmulani

Differential Revision: D6671979

fbshipit-source-id: d36933415c2fa50bae5cc171434d8f0b281955b7

React/Views/RCTShadowView+Layout.m
@@ -81,6 +81,12 @@ CGFloat RCTCoreGraphicsFloatFromYogaFloat(float value)
   return UIEdgeInsetsInsetRect((CGRect){CGPointZero, self.frame.size}, self.compoundInsets).size;
 }
 
+- (CGRect)contentFrame
+{
+  CGRect bounds = (CGRect){CGPointZero, self.frame.size};
+  return UIEdgeInsetsInsetRect(bounds, self.compoundInsets);
+}
+
 #pragma mark - Measuring
 
 - (CGSize)sizeThatFitsMinimumSize:(CGSize)minimumSize maximumSize:(CGSize)maximumSize

aleclarson/react-native-macos

React/Views/RCTShadowView+Layout.m
@@ -89,6 +89,12 @@ static inline CGRect NSEdgeInsetsInsetRect(CGRect rect, NSEdgeInsets insets) {
   return  NSEdgeInsetsInsetRect((CGRect){CGPointZero, self.frame.size}, self.compoundInsets).size;
 }
 
+- (CGRect)contentFrame
+{
+  CGRect bounds = (CGRect){CGPointZero, self.frame.size};
+  return NSEdgeInsetsInsetRect(bounds, self.compoundInsets);
+}
+
 #pragma mark - Measuring
 
 - (CGSize)sizeThatFitsMinimumSize:(CGSize)minimumSize maximumSize:(CGSize)maximumSize